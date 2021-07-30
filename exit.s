d31020efa4fcaf4b9673f61a3eee674f54609ee2
Mirhat yasar <40506686+Mirhatyasar@users.noreply.github.com>
2021-07-30 13:19:27 +0300

    exit program

---
+++ exit.s
@@ -0,0 +1,14 @@
+.seciton .data
+
+.section .text
+.globl _start
+
+_start:
+ movl $1, %eax
+ movl $0, %ebx
+ 
+ int $0x80
+ 
+ # to run the program: as exit.s -o exit.o && ld exit.o -o exit
+ # ./exit
+ # to see the result: echo $?

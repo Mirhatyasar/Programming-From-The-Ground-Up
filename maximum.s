e14fab6e7cc1505a8174241cfc0b6f07967102ba
Mirhat yasar <40506686+Mirhatyasar@users.noreply.github.com>
2021-07-30 13:11:09 +0300

    find the largest number
    first program

---
+++ maximum.s
@@ -0,0 +1,28 @@
+.section .data
+ .data_items: 55,66,88,99,31,30,57,89,33,211,28,20,0
+ 
+ .section .text
+ .globl _start
+ 
+ _start:
+  movl $0, %edi
+  movl data_items(,%edi,4), %eax
+  movl %eax, %ebx
+  
+start_loop:
+ cmpl $0, %eax
+ je exit_loop
+ incl %edi
+ movl data_items(,%edi,4), %eax
+ cmpl %ebx, %eax
+ jle start_loop
+ movl %eax, %ebx
+ 
+exit_loop:
+ movl $1, %eax
+ int $0x80
+ 
+ # to run the program:
+ # as maxiumum.s -o maxiumum.o && ld maximum.o -o maxiumum
+ # ./maxiumum
+ # to see the result echo $?
